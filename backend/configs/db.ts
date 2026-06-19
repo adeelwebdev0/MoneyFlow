ddcd7b838053a345d79e37dfaf0bf0caa2cfbc78
adeelwebdev0 <adeelwebdev0@gmail.com>
2026-06-19 15:00:21 +0500

    startign writing model

--- backend/configs/db.ts
+++ backend/configs/db.ts
@@ -1,1 +1,20 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const connectDb = async (): Promise<void> => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI as string);
+    console.log("Database is connected ✅");
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.log(error.message);
+    } else {
+      console.log("Unknown DB connection error");
+    }
+    process.exit(1);
+  }
+};
+
+export default connectDb;
--- backend/server.ts
+++ backend/server.ts
@@ -1,6 +1,15 @@
-import express from "express";
+import express, { application } from "express";
 import cors from "cors";
-
+import connectDb from "./configs/db";
 import dotenv from "dotenv";
 
 dotenv.config();
+const PORT = process.env.PORT || 5000;
+await connectDb();
+
+const app = express();
+app.use(cors());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on this ${PORT}🤝`);
+});
