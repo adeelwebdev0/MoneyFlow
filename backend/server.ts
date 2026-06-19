e7a3d14b49518ba85f025c063c4bfd4ac882e04a
adeelwebdev0 <adeelwebdev0@gmail.com>
2026-06-19 19:40:38 +0500

    starting frontend

--- backend/server.ts
+++ backend/server.ts
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import connectDb from "./configs/db";
 import transactionRoutes from "./routes/transactionRoutes";
-
+import errorHandler from "./middlewares/errorHandler";
 const PORT = process.env.PORT || 5000;
 connectDb();
 
@@ -12,7 +12,8 @@ app.use(cors());
 
 app.use(express.json());
 
-app.use("/api", transactionRoutes);
+app.use("/api/transactions", transactionRoutes);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running on this ${PORT}🤝`);
